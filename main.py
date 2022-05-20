e4a847af93ce222eb4e6e3aa90d9c3b0de1184cc
PringlePrangle <90445819+PringlePrangle@users.noreply.github.com>
2022-05-20 09:19:06 +0100

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,1 +1,96 @@
+def on_up_pressed():
+    mySprite.y += 10
+    animation.run_image_animation(mySprite, assets.animation("""
+        walkleft
+    """), 200, False)
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+def on_left_pressed():
+    mySprite.x += -10
+    animation.run_image_animation(mySprite,
+        [img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """)],
+        500,
+        False)
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_right_pressed():
+    mySprite.x += 10
+    animation.run_image_animation(mySprite,
+        [img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """)],
+        500,
+        False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_down_pressed():
+    mySprite.y += -10
+    animation.run_image_animation(mySprite,
+        [img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """)],
+        500,
+        False)
+controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
+
+mySprite: Sprite = None
+myDart = darts.create(assets.image("""
+    myImage
+"""), SpriteKind.projectile)
+mySprite = sprites.create(assets.image("""
+    player
+"""), SpriteKind.player)
+mySprite.set_scale(0.4, ScaleAnchor.MIDDLE)
+myMinimap = minimap.minimap(MinimapScale.ORIGINAL, 2, 2)
+minimap.include_sprite(myMinimap, mySprite, MinimapSpriteScale.MINIMAP_SCALE)
+
+def on_forever():
+    controller.move_sprite(mySprite, 50, 50)
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,63 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.y += -1
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim1`,
+    200,
+    false
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.x += 1
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walkright`,
+    200,
+    false
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.x += -1
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walkleft`,
+    200,
+    false
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.y += 1
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim2`,
+    200,
+    false
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+	
+})
+let snek: Sprite = null
 let mySprite: Sprite = null
-let myMinimap = minimap.minimap(MinimapScale.Half, 2, 0)
-minimap.includeSprite(myMinimap, mySprite)
+mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
+mySprite.setScale(0.6, ScaleAnchor.Middle)
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.floorDark1)
+scene.cameraFollowSprite(mySprite)
+let tarketup = sprites.create(assets.image`targettop`, SpriteKind.Food)
+tiles.placeOnRandomTile(tarketup, assets.tile`myTile5`)
+let targetdown = sprites.create(assets.image`targetdown`, SpriteKind.Food)
+tiles.placeOnRandomTile(targetdown, assets.tile`myTile7`)
+let targetleft = sprites.create(assets.image`targetleft`, SpriteKind.Food)
+tiles.placeOnRandomTile(targetleft, assets.tile`myTile6`)
+let targetright = sprites.create(assets.image`targetright`, SpriteKind.Food)
+tiles.placeOnRandomTile(targetright, assets.tile`myTile4`)
+if (randint(1, 4) == 1) {
+    snek = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(snek, sprites.dungeon.floorDark1)
+    snek.setVelocity(200, 200)
+    snek.follow(tarketup)
+}
+forever(function () {
+    controller.moveSprite(mySprite, 50, 50)
+})
